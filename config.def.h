7e0a15b5950c567beaa2b62ac076e715142872d3
Vaibhav Dhiman <mousemoosz@protonmail.com>
2022-06-21 14:23:51 +0530

    change terminal to alacritty

--- config.def.h
+++ config.def.h
@@ -52,7 +52,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -67,8 +67,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *shutdowncmd[]  = { "shutdown", "now", NULL };
 static const char *rebootcmd[]  = { "shutdown", "-r", "now", NULL };
-static const char *termcmd[]  = { "tabbed", "-r", "2", "st", "-w", "''", "-e", "tmux",  NULL };
-static const char *roficmd[] = { "rofi", "-show", "run", NULL};
+static const char *termcmd[]  = { "alacritty",  NULL };
 
 #include "maximize.c"
 static Key keys[] = {
